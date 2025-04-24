37ab1dc6a61867d97e9d64a04d19915b020ea9e0
Jubair Ibn Khaled <160339037+Juba1r@users.noreply.github.com>
2025-04-24 17:18:13 +0600

    some css

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+// import hero_img from
 
 export default function Home() {
   return (
-   <div>
-    Home
-    
-   </div>
+    <div>
+      <Image src={hero_img} />
+    </div>
   );
 }
-
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import styles from "./footer.module.css";
+import Image from "next/image";
+
 
 const Footer = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <div> 2025 WebApp. All Rights Reserved.</div>
-      <div></div>
+      <div className={styles.social}>
+        <Image src="/1.png" className={styles.icon} width={15} height={15} alt="web app" />
+        <Image src="/2.png" className={styles.icon} width={15} height={15} alt="web app" />
+        <Image src="/3.png" className={styles.icon} width={15} height={15} alt="web app" />
+        <Image src="/4.png" className={styles.icon} width={15} height={15} alt="web app" />
+      </div>
     </div>
   );
 };
